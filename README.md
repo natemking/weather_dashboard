b803040f1a48924876b37aeca078fccac4f5c48f
Nate M King <72567521+natemking@users.noreply.github.com>
2020-12-07 21:09:14 -0500

    Local storage funcitonality built

--- README.md
+++ README.md
@@ -1,2 +1,63 @@
 # weather_dashboard
 Homework #06 Server-Side APIs: Weather Dashboard
+
+---
+
+## Table of Contents
+ * [Description](#description)
+    + [Scope of Work](#scope-of-work)
+    + [HTML and CSS](#html-and-css)
+    + [Javascript functionality](#javascript-functionality)
+  * [Screenshots](#screenshots)
+  * [Credits](#credits)
+
+## Description
+---
+### Scope of Work
+
+
+### HTML and CSS
+   
+
+### JavaScript Functionality
+
+
+---
+
+## Screenshots
+
+<!-- <summary><strong>Work Day Planner</strong></summary>
+<br>
+
+
+![workday planner day add entries](./assets/images/screenshots/work_day_planner_add.gif?raw=true)
+<br>
+_add entries_
+<br>
+
+![workday planner clear schedule](./assets/images/screenshots/work_day_planner_clear.gif?raw=true)
+<br>
+_clear schedule_
+<br>
+
+![workday planner hour change](./assets/images/screenshots/work_day_planner_hour_change.gif?raw=true)
+<br>
+_hour change_
+<br>
+
+![workday planner day change](./assets/images/screenshots/work_day_planner_day_change.gif?raw=true)
+<br>
+_day change_
+<br> -->
+
+---
+
+## Credits
+
+<!-- * [jQuery](https://jquery.com/)
+
+* [day.js](https://day.js.org/)
+
+* [Get value of list item](https://stackoverflow.com/questions/5548827/get-value-of-list-item-with-jqueryp)
+
+* -->
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,3 +1,48 @@
 $(document).ready(function (){
+    //Search for locale
+        //Add its current weather data to the jumbotron
+        //Add its five day forecast to the cards
+            //one day per card
+        //Save the cit to local storage
+            //write that locale to the visited cites list
     
+    //Get searched locale data from local stor
+    let getLocale = JSON.parse(localStorage.getItem('searched-locale'));
+        if (getLocale === null){
+            getLocale = [];
+        };
+    console.log(getLocale);
+    //For loop to iterate through stored cites and write them to DOM in the <ul> element
+   for (let i = 0; i < getLocale.length; i++) {
+    $('<li>').addClass('list-group-item').text(getLocale[i]).appendTo('#searched-locale');  
+   }
+
+   //Button click to save searched for locale and access api data
+    $('#search-btn').on('click', function () {
+        //variable for inputs value
+        let locale = $('#locale-input').val();
+        if (locale === ''){
+            alert("Please add a location")
+        }else{
+            //push the new locale to the array of previous Locale from local stor
+            getLocale.push(locale);
+        }
+        //Set locale array to 
+        localStorage.setItem('searched-locale', JSON.stringify(getLocale));
+        $('<li>').addClass('list-group-item').text(locale).appendTo('#searched-locale');
+        
+    });
+
+
+    // Saving for later 
+    // $('#searched-Locale li').on('click', function(){
+    //     test = $(this).text();
+    
+    //     console.log(test);
+    // })
+   
+
+    
+
+
 });
